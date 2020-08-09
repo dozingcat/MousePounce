c77ccf4cb7e78ca4593c98b487f7482789192b69
Brian Nenninger <bnenning@gmail.com>
2020-08-09 15:32:40 -0400

    fix title

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Egyptian Mouse Pounce',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
